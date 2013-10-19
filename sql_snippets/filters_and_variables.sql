da4fa3a18a6744dd58e3499a1b8b8f413a811607
Nadav Kavalerchik <nadavkav@gmail.com>
2013-10-19 17:44:04 +0200

    Update filters_and_variables.sql

--- sql_snippets/filters_and_variables.sql
+++ sql_snippets/filters_and_variables.sql
@@ -5,17 +5,17 @@ Filters and variables ###
 %%DEBUG%% - Display the SQL query
 
 And Special filters (if available!):
-%%FILTER_SEARCHTEXT:table.field:('<', '>', '<=', '>=', '~')%%
+%%FILTER_SEARCHTEXT:table.field:('=', '<', '>', '<=', '>=', '~')%%
 %%FILTER_SEMESTER:table.field%%
 %%FILTER_YEARNUMERIC:table.field%%
-%%FILTER_YEARHEBREW:table.field%%
-%%FILTER_COURSES:table.field%%
-%%FILTER_MYCOURSE:table.field%%
-%%FILTER_CATEGORIES:table.field%%
-%%FILTER_SUBCATEGORIES:table.field%%
-%%FILTER_ROLE:table.field%%
+%%FILTER_YEARHEBREW:table.field%% 
+%%FILTER_COURSES:table.field%% - courseid
+%%FILTER_MYCOURSE:table.field%% - courseid
+%%FILTER_CATEGORIES:table.field%% - categoryid
+%%FILTER_SUBCATEGORIES:table.field%% - categoryid
+%%FILTER_ROLE:table.field%% - roleid
 %%FILTER_STARTTIME:l.time:>%% %%FILTER_ENDTIME:l.time:<%% ('<', '>', '<=', '>=', '~')
 %%FILTER_COURSEMODULEID%% , %%FILTER_COURSEMODULEFIELDS%% , %%FILTER_COURSEMODULE%%
-%%FILTER_USERS:table.field%%
-%%FILTER_SYSTEMUSER:table.field%%
-%%FILTER_COURSEUSER:table.field%%
+%%FILTER_USERS:table.field%% - Custom profile fields
+%%FILTER_SYSTEMUSER:table.field%% - userid
+%%FILTER_COURSEUSER:table.field%% - userid
