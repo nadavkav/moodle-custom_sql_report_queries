feca6e631dea32b2f9529900374a3153f18b75b5
Nadav Kavalerchik <nadavkav@gmail.com>
2013-11-05 10:29:49 +0200

    Create enrolled_users_by_enrollemt_plugin.sql

---
+++ users/enrolled_users_by_enrollemt_plugin.sql
@@ -0,0 +1,16 @@
+LIST of all site USERS by COURSE enrollment ###
+LIST of all site USERS by COURSE enrollment (Moodle 2.x) ###
+
+SELECT
+user2.firstname AS Firstname,
+user2.lastname AS Lastname,
+user2.email AS Email,
+user2.city AS City,
+course.fullname AS Course
+,(SELECT shortname FROM prefix_role WHERE id=en.roleid) AS ROLE
+,(SELECT name FROM prefix_role WHERE id=en.roleid) AS RoleName
+ 
+FROM prefix_course AS course 
+JOIN prefix_enrol AS en ON en.courseid = course.id
+JOIN prefix_user_enrolments AS ue ON ue.enrolid = en.id
+JOIN prefix_user AS user2 ON ue.userid = user2.id
