a7e4a26c6a383518e325571f9468c49d0e9c6aac
Nadav Kavalerchik <nadavkav@gmail.com>
2014-03-09 02:01:36 +0200

    Create survey.sql

---
+++ meital/survey.sql
@@ -0,0 +1,27 @@
+Meital 2014 Survey queries ###
+Meital 2014 Survey queries ###
+
+# Count courses with synchronious meetings
+SELECT m.name, COUNT(*) 
+FROM mdl_course_modules AS cm
+JOIN mdl_modules AS m ON m.id = cm.module
+WHERE m.name IN ('sookooroo', 'adobe', 'elluminate', 'webex', 'bigbluebutton')
+GROUP BY m.id
+
+# Student count in courses dividied by enrollment count 40, <200, <500, >500
+SELECT c.id, c.fullname,
+(SELECT COUNT( DISTINCT ra.userid ) AS Users
+FROM mdl_role_assignments AS ra
+JOIN mdl_context AS ctx ON ra.contextid = ctx.id AND ctx.contextlevel = 50
+WHERE ra.roleid = 5 AND ctx.instanceid = c.id) AS "Students"
+FROM mdl_course AS c
+HAVING Students < 40
+#HAVING Students < 200 AND Students > 40
+#HAVING Students < 500 AND Students > 200
+#HAVING Students > 500 
+
+# Activities and Resources count in courses, system wide.
+SELECT m.name, COUNT(*) 
+FROM mdl_course_modules AS cm
+JOIN mdl_modules AS m ON m.id = cm.module
+GROUP BY m.id
