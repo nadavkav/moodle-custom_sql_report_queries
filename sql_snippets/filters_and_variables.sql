0305939951297f6e786a7774c308808a8c2dc66a
Nadav Kavalerchik <nadavkav@gmail.com>
2014-06-17 11:21:43 +0300

    Update filters_and_variables.sql

--- sql_snippets/filters_and_variables.sql
+++ sql_snippets/filters_and_variables.sql
@@ -25,3 +25,12 @@ FROM mdl_course AS c
 JOIN mdl_course_categories AS cc ON cc.id = c.category
 WHERE 1=1
 %%FILTER_SUBCATEGORIES:cc.path%%
+
+Link to user's edit profile
+CONCAT('<a target="_new" href="%%WWWROOT%%/user/editadvanced.php?id=', 
+	   u.id, '">', u.firstname, ' ', u.lastname, '</a>') AS Fullname
+	   
+Link to course
+CONCAT('<a target="_new" href="%%WWWROOT%%/course/view.php?id=', 
+    c.id, '">', c.fullname,'</a>') AS Course
+
